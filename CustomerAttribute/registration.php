cff21407447fb0052cd25975a3f7469bab671b7d
Wishusucess <90965189+Wishusucess@users.noreply.github.com>
2021-11-20 23:50:29 +0530

    Add files via upload

---
+++ CustomerAttribute/registration.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Category: Wishusucess_CustomerAttribute',
+ * Developer: Hemant Singh Magento 2x Developer
+ * Website: http://wwww.wishusucess.com
+ */
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'Wishusucess_CustomerAttribute',
+    __DIR__
+);
